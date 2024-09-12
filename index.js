5b998f633aac7e3c9b0a7be6078badac17a1b603
Mirella Callage <mcallagedev@gmail.com>
2024-09-11 23:22:41 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -54,8 +54,24 @@ const metasRealizadas = async () => {
     }
 
     await select ({
-        message: "Metas Realizadas",
+        message: "Metas Realizadas: " + realizadas.length,
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return !meta.checked
+    })
+
+    if(abertas.length == 0) {
+        console.log("Não existem metas abertas! :)")
+        return
+    }
+
+    await select ({
+        message: "Metas Abertas: " + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -81,6 +97,11 @@ const start = async () => {
                 },
 
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -100,6 +121,9 @@ const start = async () => {
                 break
             case "realizadas":
                 await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
                 break
             case "sair":
                 return
